dc3a2458d21dcbc5029acd21597da068e43e84bd
calida21 <65916870+calida21@users.noreply.github.com>
2021-02-14 09:40:38 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class doctor(models.Model):
+    
+    full_name=models.CharField(max_length=100)
+    hospital_name=models.CharField(max_length=100)
+    number_doc=models.CharField(max_length=10)
+    # doc_id=models.IntegerField()
+    
+    user_doc=models.OneToOneField(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user_doc.username
+    
+
+class patient(models.Model):
+    name=models.CharField(max_length=20)
+    surname=models.CharField(max_length=20)
+    patient_id=models.CharField(max_length=10, null=True)
+    address=models.CharField(max_length=100)
+    number=models.CharField(max_length=10)
+    emergency_number=models.CharField(max_length=10)
+    age=models.IntegerField()
+    health_condition=models.CharField(max_length=100)
+    gender=models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    alternate_email=models.CharField(max_length=100)
+    
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    doctorw=models.ForeignKey(doctor, on_delete=models.CASCADE, null=True, related_name="help")
+
+    def __str__(self):
+        return self.user.username
+
+
+class patientsymptoms(models.Model):
+    btemp=models.IntegerField()
+    sympt1=models.CharField(max_length=100,null=False)
+    patient_fkey=models.ForeignKey(patient,on_delete=models.CASCADE,null=True)
+
+    # def __str__(self):
+    #     return self.patient_fkey.email
+
+
+
